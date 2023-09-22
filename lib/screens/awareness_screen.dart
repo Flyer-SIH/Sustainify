3172308dc60846a2dc4768f7ce37e469d08bf1e7
dakshvasudev <daksh.vasudev@suraasa.com>
2023-09-22 18:11:03 +0530

    blogs page setup

--- lib/screens/awareness_screen.dart
+++ lib/screens/awareness_screen.dart
@@ -9,74 +9,81 @@ class AwarenessScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Explore'),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              BlogCard(),
-            ],
-          ),
-        ),
+      body: ListView.builder(
+        itemCount: 4,
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, index) {
+          return BlogCard(
+            assetImage: '',
+            heading: '',
+            content: '',
+          );
+        },
       ),
     );
   }
 }
 
 class BlogCard extends StatelessWidget {
-  const BlogCard({
-    super.key,
-  });
+  const BlogCard({super.key, required this.assetImage, required this.heading, required this.content});
+
+  final String assetImage;
+  final String heading;
+  final String content;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(32),
-          border: Border.all(color: Colors.transparent),
-          boxShadow: const [
-            BoxShadow(
-              color: Colors.grey,
-              blurRadius: 6.0,
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 16),
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(32),
+            border: Border.all(color: Colors.transparent),
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.grey,
+                blurRadius: 6.0,
+              ),
+            ]),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 80,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(32),
+                border: Border.all(color: Colors.transparent),
+                image: DecorationImage(image: AssetImage(assetImage), fit: BoxFit.fill),
+              ),
             ),
-          ]),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            height: 80,
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.circular(32),
-              border: Border.all(color: Colors.transparent),
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Heading',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-                ),
-                SizedBox(
-                  height: 8,
-                ),
-                Text(
-                  'jsdngkjfdbskj jkfgb nfjjg nweigrk jnwrlkgj nerw kjgnerlkgj rtg irtgh rwtng krejubn lrwkbn rtwgn lwrkjbn rwtljbn wrkergjbn werjkljbn wrliubun wjklbn wfel bnerklwgn werkjn',
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 2,
-                  softWrap: true,
-                  style: TextStyle(
-                    fontSize: 14,
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    heading,
+                    style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
                   ),
-                ),
-              ],
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    content,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                    softWrap: true,
+                    style: const TextStyle(
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
