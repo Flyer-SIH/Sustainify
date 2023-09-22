7ddfde5bc14fe0ae04f13d2a4ca34ee6886550de
dakshvasudev <daksh.vasudev@suraasa.com>
2023-09-22 18:04:42 +0530

    code fixes

--- lib/screens/awareness_screen.dart
+++ lib/screens/awareness_screen.dart
@@ -3,70 +3,80 @@ import 'package:flutter/material.dart';
 class AwarenessScreen extends StatelessWidget {
   const AwarenessScreen({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Awareness'),
+        title: const Text('Explore'),
       ),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
             children: [
-              Container(
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(24),
-                    border: Border.all(color: Colors.transparent),
-                    boxShadow: const [
-                      BoxShadow(
-                        color: Colors.grey,
-                        blurRadius: 6.0,
-                      ),
-                    ]),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      height: 80,
-                      decoration: BoxDecoration(
-                        color: Colors.blue,
-                        borderRadius: BorderRadius.circular(24),
-                        border: Border.all(color: Colors.transparent),
-                      ),
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Heading',
-                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Text(
-                            'jsdngkjfdbskj jkfgb nfjjg nweigrk jnwrlkgj nerw kjgnerlkgj rtg irtgh rwtng krejubn lrwkbn rtwgn lwrkjbn rwtljbn wrkergjbn werjkljbn wrliubun wjklbn wfel bnerklwgn werkjn',
-                            overflow: TextOverflow.fade,
-                            maxLines: 2,
-                            softWrap: false,
-                            style: TextStyle(
-                              fontSize: 14,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+              BlogCard(),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class BlogCard extends StatelessWidget {
+  const BlogCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(32),
+          border: Border.all(color: Colors.transparent),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.grey,
+              blurRadius: 6.0,
+            ),
+          ]),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: 80,
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.circular(32),
+              border: Border.all(color: Colors.transparent),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Heading',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  'jsdngkjfdbskj jkfgb nfjjg nweigrk jnwrlkgj nerw kjgnerlkgj rtg irtgh rwtng krejubn lrwkbn rtwgn lwrkjbn rwtljbn wrkergjbn werjkljbn wrliubun wjklbn wfel bnerklwgn werkjn',
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                  softWrap: true,
+                  style: TextStyle(
+                    fontSize: 14,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
