851944e6ebc76a48f6c7f2bbdabf2d9dbb166817
dakshvasudev <daksh.vasudev@suraasa.com>
2023-09-22 23:18:45 +0530

    product page

---
+++ lib/dummy_data/product_dummy_data.dart
@@ -0,0 +1,25 @@
+import 'package:sustainify/models/product_model.dart';
+
+List<Product> dummyHouseholdProducts = [
+  Product(heading: 'Dish Soap', quantity: 3, state: 'New'),
+  Product(heading: 'Laundry Detergent', quantity: 2, state: 'New'),
+  Product(heading: 'Toilet Paper', quantity: 12, state: 'New'),
+  Product(heading: 'Paper Towels', quantity: 6, state: 'New'),
+  Product(heading: 'Trash Bags', quantity: 4, state: 'New'),
+  Product(heading: 'Broom', quantity: 1, state: 'New'),
+  Product(heading: 'Mop', quantity: 1, state: 'New'),
+  Product(heading: 'Vacuum Cleaner', quantity: 1, state: 'New'),
+  Product(heading: 'Dishwasher Tablets', quantity: 2, state: 'New'),
+];
+
+List<Product> dummyAgriculturalProducts = [
+  Product(heading: 'Wheat', quantity: 500, state: 'Fresh'),
+  Product(heading: 'Corn', quantity: 300, state: 'Fresh'),
+  Product(heading: 'Rice', quantity: 200, state: 'Fresh'),
+  Product(heading: 'Potatoes', quantity: 100, state: 'Fresh'),
+  Product(heading: 'Apples', quantity: 150, state: 'Fresh'),
+  Product(heading: 'Tomatoes', quantity: 250, state: 'Fresh'),
+  Product(heading: 'Carrots', quantity: 80, state: 'Fresh'),
+  Product(heading: 'Onions', quantity: 120, state: 'Fresh'),
+  Product(heading: 'Cucumbers', quantity: 90, state: 'Fresh'),
+];
--- lib/screens/best_from_waste_screen.dart
+++ lib/screens/best_from_waste_screen.dart
@@ -1,12 +1,177 @@
 import 'package:flutter/material.dart';
+import 'package:sustainify/dummy_data/product_dummy_data.dart';
 
 class BestFromWasteScreen extends StatelessWidget {
-  const BestFromWasteScreen({super.key});
+  const BestFromWasteScreen({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text("This is Best from Waste Screen")),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 32, 16, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(left: 16),
+              child: Text(
+                'Agricultural',
+                style: TextStyle(
+                  letterSpacing: -0.4,
+                  color: Colors.black,
+                  fontSize: 24,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 300, // Set the desired height for your product cards
+              child: ListView(
+                shrinkWrap: true,
+                children: [
+                  SizedBox(
+                    height: 270, // Set the overall height of your BestFromWasteScreen
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal, // Enable horizontal scrolling
+                      itemCount: 4,
+                      itemBuilder: (BuildContext context, int index) {
+                        return ProductDisplayCard(
+                          heading: dummyAgriculturalProducts[index].heading,
+                          state: dummyAgriculturalProducts[index].state,
+                          quantity: dummyAgriculturalProducts[index].quantity,
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 16, top: 16),
+              child: Text(
+                'Household',
+                style: TextStyle(
+                  letterSpacing: -0.4,
+                  color: Colors.black,
+                  fontSize: 24,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 300,
+              child: ListView(
+                shrinkWrap: true,
+                children: [
+                  SizedBox(
+                    height: 270,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 4,
+                      itemBuilder: (BuildContext context, int index) {
+                        return ProductDisplayCard(
+                          heading: dummyHouseholdProducts[index].heading,
+                          state: dummyHouseholdProducts[index].state,
+                          quantity: dummyHouseholdProducts[index].quantity,
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProductDisplayCard extends StatelessWidget {
+  const ProductDisplayCard({super.key, required this.heading, required this.state, required this.quantity});
+
+  final String heading;
+  final int quantity;
+  final String state;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        width: 157, // Set the desired width for your product cards
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey,
+              blurRadius: 2.0,
+            ),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 157,
+              width: 157,
+              color: Colors.red,
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8),
+              child: Text(
+                heading,
+                style: const TextStyle(
+                  letterSpacing: -0.4,
+                  color: Colors.grey,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8),
+              child: Text(
+                'Qty: $quantity',
+                style: const TextStyle(
+                  letterSpacing: -0.4,
+                  color: Colors.black,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8, bottom: 8),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.grey,
+                    border: Border.all(color: Colors.transparent),
+                    borderRadius: BorderRadius.circular(6)),
+                padding: const EdgeInsets.all(4),
+                child: Text(
+                  state,
+                  style: const TextStyle(
+                    letterSpacing: -0.4,
+                    color: Colors.white,
+                    fontSize: 12,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
