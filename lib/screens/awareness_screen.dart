d57dffc66e6aa7cf9dca9619a60ce8a0aa9db0bb
dakshvasudev <daksh.vasudev@suraasa.com>
2023-09-22 17:54:16 +0530

    awareness_screen.dart initial

--- lib/screens/awareness_screen.dart
+++ lib/screens/awareness_screen.dart
@@ -3,10 +3,71 @@ import 'package:flutter/material.dart';
 class AwarenessScreen extends StatelessWidget {
   const AwarenessScreen({super.key});
 
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text("This is Awareness Screen")),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Awareness'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(24),
+                    border: Border.all(color: Colors.transparent),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.grey,
+                        blurRadius: 6.0,
+                      ),
+                    ]),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: 80,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(24),
+                        border: Border.all(color: Colors.transparent),
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Heading',
+                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            'jsdngkjfdbskj jkfgb nfjjg nweigrk jnwrlkgj nerw kjgnerlkgj rtg irtgh rwtng krejubn lrwkbn rtwgn lwrkjbn rwtljbn wrkergjbn werjkljbn wrliubun wjklbn wfel bnerklwgn werkjn',
+                            overflow: TextOverflow.fade,
+                            maxLines: 2,
+                            softWrap: false,
+                            style: TextStyle(
+                              fontSize: 14,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
