9d9006f700a42e90a6a8f04f4cb346201af19d78
dakshvasudev <daksh.vasudev@suraasa.com>
2023-09-23 10:55:49 +0530

    bug fixes

--- lib/dummy_data/articles_data.dart
+++ lib/dummy_data/articles_data.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:sustainify/models/blog_model.dart';
 
 List<Detail> details = [
--- lib/screens/blog_detail_screen.dart
+++ lib/screens/blog_detail_screen.dart
@@ -13,43 +13,45 @@ class BlogDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Learn in Detail'),
       ),
-      body: Column(
-        children: [
-          Image.network(
-            networkImage,
-          ),
-          const SizedBox(
-            height: 8,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              heading,
-              style: const TextStyle(
-                color: Colors.black,
-                fontSize: 22,
-                fontWeight: FontWeight.w700,
-                letterSpacing: -0.4,
-                height: 1.3,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(
+              networkImage,
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                heading,
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 22,
+                  fontWeight: FontWeight.w700,
+                  letterSpacing: -0.4,
+                  height: 1.3,
+                ),
               ),
             ),
-          ),
-          const SizedBox(
-            height: 8,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              content,
-              style: const TextStyle(
-                color: Colors.black,
-                fontSize: 18,
-                fontWeight: FontWeight.w400,
-                letterSpacing: -0.36,
-              ),
+            const SizedBox(
+              height: 8,
             ),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                content,
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: -0.36,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
